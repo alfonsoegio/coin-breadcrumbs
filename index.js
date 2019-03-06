167e93bcb2b2b9fb03f940073a6da788537f2f17
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-06 20:42:20 +0100

    Add txPos resolver

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const txref = require('./lib/txref.js')
 const chainClient = require('./lib/chainClient.js')
 
 const main = async function () {
-  console.log(txref.encode('mainnet', 1400000, 3, 2))
-  console.log(txref.decode(txref.encode('testnet', 1400000, 3, 2)))
-  console.log(await chainClient.getTx('03ab7086dd9fc7c92df5288238e94871051129f6341f22ab6612da15443a3096'))
+  let txId = '19d524a556849264018206a3b4640110dd57906fdf564dc5d60b271f178a35be'
+  let tx = await chainClient.getTx(txId)
+  let blockHash = tx.data.blockhash
+  console.log(tx.data.block_no)
+  let txPos = await chainClient.getTxPosition(txId, blockHash)
+  let did = txref.encode('testnet', tx.data.block_no, txPos, 0)
+  console.log(did)
+  console.log(txref.decode(did))
 }
 
 main()
