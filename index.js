ba73ed2ff0f5b876f34a13648d3c90bd78144ec5
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-06 19:46:11 +0100

    Add chainClient

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const txref = require('./lib/txref.js')
+const chainClient = require('./lib/chainClient.js')
 
-console.log(txref.encode('mainnet', 1400000, 3, 2))
-console.log(txref.decode(txref.encode('testnet', 1400000, 3, 2)))
+const main = async function () {
+  console.log(txref.encode('mainnet', 1400000, 3, 2))
+  console.log(txref.decode(txref.encode('testnet', 1400000, 3, 2)))
+  console.log(await chainClient.getTx('03ab7086dd9fc7c92df5288238e94871051129f6341f22ab6612da15443a3096'))
+}
+
+main()
