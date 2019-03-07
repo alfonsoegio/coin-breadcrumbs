a278a8f78e2d46da5f7b70a6e9d773c46406b682
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-07 18:48:24 +0100

    Wallet as object

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Wallet = require('./lib/wallet.js')
 
 const main = async function () {
   let myWallet = new Wallet('.wallet/keys.json')
-  await myWallet.refreshWalletInfo()
+  myWallet.refreshWalletInfo()
 }
 
 main()
