dba845bcd4ef6d9d5f661e8a27fd1d3ffe5fdced
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-07 17:05:32 +0100

    Include filename

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Wallet = require('./lib/wallet.js')
 
 const main = async function () {
   let myWallet = new Wallet('.wallet/keys.json')
-  myWallet.refreshWalletInfo()
+  await myWallet.refreshWalletInfo()
 }
 
 main()
