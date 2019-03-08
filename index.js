d8b5f6145dcfb2448062ac7ab4c19976586db66d
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-08 14:33:52 +0100

    Add proper throttling

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Wallet = require('./lib/wallet.js')
 
+const FILENAME = '.wallet/keys.json'
+
 const main = async function () {
-  let myWallet = new Wallet('.wallet/keys.json')
-  myWallet.refreshWalletInfo()
+  let myWallet = new Wallet(FILENAME)
+  await myWallet.refreshWalletInfo()
+  await setTimeout(() => console.log(myWallet), 40000)
+  console.log(JSON.stringify(myWallet, null, 2))
 }
 
 main()
