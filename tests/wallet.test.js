91b76e0b2bee6575534b1b858c28634195377aac
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-08 14:51:33 +0100

    Return if file already exists

--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -1,10 +1,21 @@
+const fs = require('fs')
 const Wallet = require('../lib/wallet.js')
 require('chai').should()
 
+const FILENAME = '.wallet/test.json'
+
 it('Should be able to build a Wallet', function () {
   let mnemonic = 'question flock gloom frequent fog grief ticket glory beef truly settle suffer'
-  let wallet = new Wallet('.wallet/test.json', mnemonic, 4)
+  let wallet = new Wallet(FILENAME, mnemonic, 4)
   wallet.keys.key.should.be.an('array')
   wallet.keys.key.length.should.be.a('number')
   wallet.keys.key.length.should.equal(4)
+  wallet.keys.key[0].should.be.an('object')
+  wallet.keys.key[0].wif.should.be.a('string')
+  wallet.keys.key[0].wif.should.equal('cVaapSC8iinubjXcVHBnf8hTKsKiwLnzqmB6NzHE8n3UMUTRCmf7')
+  wallet.keys.key[0].address.should.be.a('string')
+  wallet.keys.key[0].address.should.equal('msbXLN3KsvKQmPF8vc87EDUKuczM5HWEjc')
+  let secondWallet = new Wallet(FILENAME)
+  wallet.should.eql(secondWallet)
+  fs.unlinkSync(FILENAME)
 })
