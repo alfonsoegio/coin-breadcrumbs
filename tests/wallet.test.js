ea11efcf607ead2f4964b63c1c559f0d2d9c9a07
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-08 15:42:43 +0100

    Shorter tests

--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -3,10 +3,11 @@ const Wallet = require('../lib/wallet.js')
 require('chai').should()
 
 const FILENAME = '.wallet/test.json'
+const MNEMONIC = 'question flock gloom frequent fog grief ticket glory beef truly settle suffer'
+const DEFAULT_ADDRESS_NO = 3
 
 it('Should be able to build a Wallet', function () {
-  let mnemonic = 'question flock gloom frequent fog grief ticket glory beef truly settle suffer'
-  let wallet = new Wallet(FILENAME, mnemonic, 4)
+  let wallet = new Wallet(FILENAME, MNEMONIC, DEFAULT_ADDRESS_NO)
   wallet.keys.key.should.be.an('array')
   wallet.keys.key.length.should.be.a('number')
   wallet.keys.key.length.should.equal(4)
@@ -21,8 +22,7 @@ it('Should be able to build a Wallet', function () {
 })
 
 it('Should be able to read a Wallet from a file and retrieve utxo info', async function () {
-  let mnemonic = 'question flock gloom frequent fog grief ticket glory beef truly settle suffer'
-  let wallet = new Wallet(FILENAME, mnemonic, 4)
+  let wallet = new Wallet(FILENAME, MNEMONIC, DEFAULT_ADDRESS_NO)
   await wallet.refreshWalletInfo()
   wallet.keys.key.map((x) => x.info.should.be.an('object'))
   wallet.keys.key.map((x) => x.info.should.include.key('status'))
