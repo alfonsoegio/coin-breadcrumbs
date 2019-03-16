cd9ca5a10d40ee9c01b2cf6255d01947b8c6229a
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-16 12:53:27 +0100

    Recursively resolving

--- tests/coinFeed.test.js
+++ tests/coinFeed.test.js
@@ -2,9 +2,9 @@ const CoinFeed = require('../lib/coinFeed.js')
 require('chai').should()
 
 const DEFAULT_TXREF = 'txtest1:8xsj-6zzq-qpqq-8qwr-ns'
+const DEFAULT_FILENAME = '.coinFeeds/test.json'
 
 it('coinFeed should resolve a timeline given a txref', async function () {
-  let feed = new CoinFeed(DEFAULT_TXREF)
-  let timeline = await feed.resolve()
-  console.log(JSON.stringify(timeline, null, 4))
+  let feed = new CoinFeed(DEFAULT_FILENAME, DEFAULT_TXREF)
+  await feed.resolve()
 })
