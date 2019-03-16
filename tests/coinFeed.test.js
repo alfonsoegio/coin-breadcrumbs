609aa21c2cd33ea148ce50226680f3f40852f454
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-03-16 12:32:03 +0100

    Resolving coinFeed

--- tests/coinFeed.test.js
+++ tests/coinFeed.test.js
@@ -5,6 +5,6 @@ const DEFAULT_TXREF = 'txtest1:8xsj-6zzq-qpqq-8qwr-ns'
 
 it('coinFeed should resolve a timeline given a txref', async function () {
   let feed = new CoinFeed(DEFAULT_TXREF)
-  let tx = await feed.resolve()
-  tx.data.outputs.map((x) => console.log('    ' + x.script_asm))
+  let timeline = await feed.resolve()
+  console.log(JSON.stringify(timeline, null, 4))
 })
